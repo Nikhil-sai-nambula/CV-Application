cc6c5fa7ced9d7cb65532d88c6712279176b1b84
Nikhil_Sai_Nambula <nambula.nikhilsai@gmail.com>
2024-11-03 17:11:33 +0530

    Lets go buddy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/CV-Application/", // Update the base path to match the deployment URL
+  // base: "/CV-Application/", // Update the base path to match the deployment URL
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Set this to a higher value, e.g., 1000 kB
