57c495f327ec9ebef14c21542be779f27b234685
Nikhil_Sai_Nambula <nambula.nikhilsai@gmail.com>
2024-11-03 16:29:22 +0530

    Common man

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/CV-Application/", // Update the base path to match the deployment URL
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Set this to a higher value, e.g., 1000 kB
-    rollupOptions: {
-      external: ["styled-components"],
-    },
+    rollupOptions: {},
   },
 });
